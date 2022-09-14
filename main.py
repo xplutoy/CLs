07089483f407b3b5c1d5c93df2269179dce5cdb2
yx <yxue3357@qq.com>
2022-09-14 16:25:10 +0800

    test fun

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from models.er import Er
 from utils.args import add_rehearsal_args, add_experiment_args
 from backbones.simple_mlp import SimpleMLP
 from tqdm import tqdm, trange
+from continuum import Logger
 
 
 def main():
@@ -16,23 +17,32 @@ def main():
     add_rehearsal_args(parser)
     args = parser.parse_known_args()[0]
 
+    # metric
+    metric_log = Logger()
+
     # gpu
     gpu_ids = [i for i in range(torch.cuda.device_count())]
     device = torch.device(
         f'cuda:{gpu_ids[0]}' if torch.cuda.is_available() else 'cpu')
 
     mlp = SimpleMLP()
-    model = Er(mlp, device,args)
+    model = Er(mlp,
+               buffer_size=args.buffer_size,
+               lr=args.lr,
+               batch_size=args.batch_size,
+               minibatch_size=args.minibatch_size,
+               device=device,
+               metric_log=metric_log
+               )
+
     n_tasks = permut_mnist_scenarios['train'].nb_tasks
+    train_scenario = permut_mnist_scenarios['train']
+    test_scenario = permut_mnist_scenarios['test']
 
     # train & test
     with trange(n_tasks, position=0) as tt:
-        for task_id, taskset in enumerate(permut_mnist_scenarios["train"]):
+        for task_id, taskset in enumerate(train_scenario):
             tt.set_description(f'Task {task_id} Processing')
-
-            # Continuum库Log的一个bug，end_task
-            if task_id != 0:
-                model.log.end_task()
 
             # task level train
             with trange(args.n_epochs, position=1, leave=False) as et:
@@ -40,14 +50,20 @@ def main():
                     et.set_description(f'Task {task_id} Epoch {epoch}')
 
                     model.observe(taskset)
-                    model.log.end_epoch()
 
                     et.update()
+
             tt.update()
 
+            # task level test 当前模型对当前任务
+            for taskset in test_scenario:
+                model.eval(test_scenario[task_id])
+
             tqdm.write(
-                f"Task id={task_id}, acc={model.log.online_accuracy}, avg_acc={model.log.online_cumulative_performance}")
-            # test
+                f'Test : Model {task_id} acc={round(metric_log.accuracy, 4)}, avg_acc={round(metric_log.average_incremental_accuracy,4)}'
+            )
+
+            metric_log.end_task()
 
 
 if __name__ == '__main__':
--- models/er.py
+++ models/er.py
@@ -1,33 +1,51 @@
+from argparse import Namespace
 import numpy as np
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
-from continuum import rehearsal
+from continuum import rehearsal,Logger
 from utils.buffer import rand_batch_get
 from utils.weight import init_weights
-from models.base import ContinualModel
 from tqdm import tqdm
 
 
-class Er(ContinualModel):
-    def __init__(self, backbone, device, args) -> None:
-        super().__init__(backbone, device, args)
+class Er(nn.Module):
+    def __init__(self,
+                 net: nn.Module,
+                 buffer_size: int,
+                 lr: float,
+                 batch_size: int,
+                 minibatch_size: int,
+                 metric_log: Logger,
+                 device: torch.device = 'cpu',
+                 ) -> None:
+        super().__init__()
 
-        self.loss_ce = torch.nn.CrossEntropyLoss()
+        self.net = net.to(device)
+        self.optimizer = torch.optim.SGD(self.net.parameters(), lr=lr)
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.minibatch_size = minibatch_size
+        self.device = device
+        self.metric_log = metric_log
+
         self.buffer = rehearsal.RehearsalMemory(
-            memory_size=args.buffer_size,
+            memory_size=buffer_size,
             herding_method="random"
         )
         init_weights(self.net)
 
     def observe(self, taskset):
+        self.net.train()
         task_loader = DataLoader(
-            taskset, batch_size=self.args.batch_size, shuffle=True)
+            taskset, batch_size=self.batch_size, shuffle=True)
 
         for x, y, t in task_loader:
 
             if len(self.buffer) != 0:
                 _x, _y, _t = rand_batch_get(
-                    self.buffer, self.args.minibatch_size)
+                    self.buffer, self.minibatch_size)
                 x = torch.cat((x, torch.from_numpy(np.expand_dims(_x, 1))))
                 y = torch.cat((y, torch.from_numpy(_y)))
                 t = torch.cat((t, torch.from_numpy(_t)))
@@ -36,17 +54,32 @@ class Er(ContinualModel):
             y = y.to(self.device)
 
             logits = self.net(x)
-            loss = self.loss_ce(logits, y)
+            loss = self.criterion(logits, y)
 
-            self.opt.zero_grad()
+            self.optimizer.zero_grad()
             loss.backward()
-            self.opt.step()
+            self.optimizer.step()
 
             preds = torch.argmax(logits, dim=-1)
             # acc = (preds == y).to(torch.float64).mean().item()
             # tqdm.write(
             #     f"task_id={self.log.nb_tasks}, online_acc = {acc}, loss = {loss.item()}")
 
-            self.log.add([preds, y, t], subset='train')
+            self.metric_log.add([preds, y, t], subset='train')
 
         self.buffer.add(*taskset.get_raw_samples(), None)
+
+    @torch.no_grad()
+    def eval(self, taskset):
+        self.net.eval()
+        task_loader = DataLoader(
+            taskset, batch_size=self.batch_size, shuffle=False)
+
+        for x, y, t in task_loader:
+
+            x = x.to(self.device)
+            y = y.to(self.device)
+
+            logits = self.net(x)
+            preds = torch.argmax(logits, dim=-1)
+            self.metric_log.add([preds, y, t], subset='test')
